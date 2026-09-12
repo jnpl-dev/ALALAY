60744568d3d46e98a2604a57ad18d022d16a0429
jnpl-dev <johnpaullaureano.neust@gmail.com>
2026-09-12 11:21:22 +0800

    Added styling to active page for better UX

---
+++ config/ziggy.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'only' => ['*'],
+    'paths' => ['api/*'],
+    'prefix' => '',
+    'domain' => null,
+    'defaults' => [],
+    'middleware' => [],
+    'absolute' => false,
+];
